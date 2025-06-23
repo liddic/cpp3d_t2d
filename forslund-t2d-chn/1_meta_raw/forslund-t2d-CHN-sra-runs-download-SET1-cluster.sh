b4a6406d0a613d7e25b760811642f3be1ab15177
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2025-06-23 17:20:31 +0930

    Add files via upload

---
+++ forslund-t2d-chn/1_meta_raw/forslund-t2d-CHN-sra-runs-download-SET1-cluster.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=2-0
+#SBATCH --mem=64000M
+#SBATCH --cpus-per-task=16
+
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341581
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341585
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341586
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341587
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341588
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341589
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341599
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341600
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341601
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341602
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341604
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341606
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341609
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341636
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341645
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341646
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341652
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341654
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341655
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341657
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341660
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341661
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341663
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341664
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341665
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341669
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341670
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341673
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341674
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341675
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341676
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341681
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341684
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341687
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341693
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR341713
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413575
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413576
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413578
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413579
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413580
+
---
+++ forslund-t2d-chn/1_meta_raw/forslund-t2d-CHN-sra-runs-download-SET2-cluster.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=2-0
+#SBATCH --mem=64000M
+#SBATCH --cpus-per-task=16
+
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413581
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413582
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413584
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413585
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413587
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413592
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413593
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413594
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413597
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413598
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413599
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413600
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413601
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413603
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413605
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413606
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413607
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413608
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413610
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413613
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413614
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413615
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413616
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413617
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413618
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413619
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413620
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413621
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413623
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413625
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413626
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413634
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413637
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413642
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413652
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413660
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413661
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413670
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413688
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413758
+fastq-dump --outdir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR413768
