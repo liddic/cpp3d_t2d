847c675c8dceb3ab1dd64c2a7e174055dc14dd72
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2025-06-23 17:25:21 +0930

    Add files via upload

---
+++ forslund-t2d-chn/2_clean_fastp/job_index/SRR341670.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --time=1-0
+#SBATCH --mem=16000
+#SBATCH --cpus-per-task=4
+#SBATCH --nodes=1
+fastp --length_required 50 --average_qual 25 --n_base_limit 1 --dedup \
+    --trim_front1 4 --trim_tail1 2 --trim_front2 4 --trim_tail2 2 \
+    --cut_mean_quality 30 --cut_window_size 5 --cut_front --cut_tail \
+    --out1 /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_2_clean_fastp/SRR341670_R1.good.fastq --unpaired1 /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_2_clean_fastp/SRR341670_R1.single.fastq \
+    --out2 /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_2_clean_fastp/SRR341670_R2.good.fastq --unpaired2 /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_2_clean_fastp/SRR341670_R2.single.fastq \
+    --in1 /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw/SRR341670_pass_1.fastq --in2 /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw/SRR341670_pass_2.fastq
+    --adapter_fasta /cluster/jobs/lidd0026/forslund-t2d-chn/IlluminaAdapters.fa \
+    --html  /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_2_clean_fastp/QC_fastq_qual_files/SRR341670_fastp.html --json /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_2_clean_fastp/QC_fastq_qual_files/SRR341670_fastp.json \
