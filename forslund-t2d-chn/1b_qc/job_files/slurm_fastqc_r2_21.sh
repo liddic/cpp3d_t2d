7acc78859420f04a91ef36f2dbfbe9e775536756
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2025-06-23 17:33:26 +0930

    Add files via upload

---
+++ forslund-t2d-chn/1b_qc/job_files/slurm_fastqc_r2_21.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+#SBATCH --ntasks=1
+#SBATCH --time=1-0
+#SBATCH --mem=16000
+#SBATCH --cpus-per-task=4
+#SBATCH --nodes=1
+fastqc -o /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1b_qc -t 4 /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_1_meta_raw/SRR341636_pass_2.fastq
