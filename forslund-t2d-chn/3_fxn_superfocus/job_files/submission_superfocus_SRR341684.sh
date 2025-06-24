856b4563975cb23bfc4b8655eb295378e100c072
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2025-06-24 16:38:23 +0930

    Add files via upload

---
+++ forslund-t2d-chn/3_fxn_superfocus/job_files/submission_superfocus_SRR341684.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --time=4-0
+#SBATCH --mem=64000M
+#SBATCH --cpus-per-task=16
+#SBATCH --nodes=1
+export TMPDIR=/cluster/jobs/lidd0026/temp/SRR341684
+superfocus -q /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_2_clean_fastp/SRR341684_R1.good.fastq -dir /cluster/jobs/lidd0026/forslund-t2d-chn/ft2d_3_fxn_superfocus/superfocus_out_SRR341684 -a diamond -db DB_100 --alternate_directory /home/lidd0026/miniconda3/lib/python3.8/site-packages/superfocus_app
